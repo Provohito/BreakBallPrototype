c35599d6e9c4dff17482d01be59d9c2842341053
Alex Semchenkov <zigola12@list.ru>
2021-12-24 16:59:53 +0300

    Commit

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerInput : MonoBehaviour
+{
+    public static event Action<float> OnMove;
+
+    private Vector2 _startPosition = Vector2.zero;
+    private float _direction = 0f;
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        OnMove?.Invoke(Input.GetAxis("Horizontal"));
+#endif
+#if UNITY_ANDROID
+        GetTouchInput();
+#endif
+    }
+
+    // Считывание тач управления
+    private void GetTouchInput()
+    {
+        if (Input.touchCount > 0) // Проверка на качание
+        {
+            Touch touch = Input.GetTouch(0);// Считываем касание
+
+            switch (touch.phase)// Какое было касание
+            {
+                /*case TouchPhase.Began:
+                    _startPosition = touch.position;
+                    _direction = 0f;
+                    break;*/
+                case TouchPhase.Moved:
+                    _direction = touch.position.x > _startPosition.x ? 1f : -1f;
+                    break;
+                /* case TouchPhase.Stationary:
+                     break;
+                 case TouchPhase.Ended:
+                     break;
+                 case TouchPhase.Canceled:
+                     break;*/
+                default:
+                    _startPosition = touch.position;
+                    _direction = 0f;
+                    break;
+            }
+
+            OnMove?.Invoke(_direction);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody2D _rb2D;
+
+    private float _moveX = 0f;
+    [SerializeField] private float _speed = 7f;
+
+
+    private void Awake()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float positionX = _rb2D.position.x + _moveX * _speed * Time.fixedDeltaTime;
+
+        _rb2D.MovePosition(new Vector2(positionX, _rb2D.position.y));
+    }
+
+    private void OnEnable()
+    {
+        PlayerInput.OnMove += Move;
+    }
+
+    private void OnDisable()
+    {
+        PlayerInput.OnMove -= Move;
+    }
+    private void Move(float moveX)
+    {
+        _moveX = moveX;
+    }
+}
