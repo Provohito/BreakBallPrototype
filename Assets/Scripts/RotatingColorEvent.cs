c73f80f5a8a7a91ce8d80a5c010fae6745c16539
Alex Semchenkov <zigola12@list.ru>
2022-02-10 15:54:42 +0300

    commit

--- Assets/Scripts/RotatingColorEvent.cs
+++ Assets/Scripts/RotatingColorEvent.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class RotatingColorEvent : MonoBehaviour
 {
+    
     private void Update()
     {
         transform.Rotate(0, 0, 1.0f);
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Die")
+            collision.gameObject.SetActive(false);
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Die")
+            collision.gameObject.SetActive(false);
     }
 }
